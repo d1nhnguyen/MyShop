de346953d74a4c37fb1e3ec79f2a314ad6f22f35
Your Name <you@example.com>
2025-12-29 21:37:07 +0700

    implementing SortStrategies test

---
+++ src/MyShop.Core/Strategies/SortStrategies.cs
@@ -0,0 +1,39 @@
+﻿using MyShop.Core.Interfaces.Strategies;
+using MyShop.Core.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyShop.Core.Strategies
+{
+    public class PriceAscSortStrategy : ISortStrategy
+    {
+        public List<Product> Sort(IEnumerable<Product> products)
+        {
+            return products.OrderBy(p => p.Price).ToList();
+        }
+    }
+
+    public class PriceDescSortStrategy : ISortStrategy
+    {
+        public List<Product> Sort(IEnumerable<Product> products)
+        {
+            return products.OrderByDescending(p => p.Price).ToList();
+        }
+    }
+
+    public class NameAscSortStrategy : ISortStrategy
+    {
+        public List<Product> Sort(IEnumerable<Product> products)
+        {
+            return products.OrderBy(p => p.Name).ToList();
+        }
+    }
+
+    public class StockDescSortStrategy : ISortStrategy
+    {
+        public List<Product> Sort(IEnumerable<Product> products)
+        {
+            return products.OrderByDescending(p => p.Stock).ToList();
+        }
+    }
+}
