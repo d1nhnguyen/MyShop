eb1f64a2893cb75c4ccf85fd0883d553462fb438
Your Name <you@example.com>
2025-12-29 21:26:12 +0700

    implementing AuthServiceTests test

---
+++ src/MyShop.Tests/Mocks/MockConfigService.cs
@@ -0,0 +1,18 @@
+﻿using MyShop.Core.Interfaces.Services;
+
+namespace MyShop.Tests.Mocks
+{
+    public class MockConfigService : IConfigService
+    {
+        private string _serverUrl = "http://localhost:4000/graphql";
+        private string _dbName = "MyShopDb";
+
+        public string GetServerUrl() => _serverUrl;
+
+        public void SaveServerUrl(string url) => _serverUrl = url;
+
+        public string GetDatabaseName() => _dbName;
+
+        public void SaveDatabaseName(string dbName) => _dbName = dbName;
+    }
+}
---
+++ src/MyShop.Tests/Mocks/MockSessionManager.cs
@@ -0,0 +1,26 @@
+﻿using MyShop.Core.Interfaces.Services;
+using MyShop.Core.Models;
+
+namespace MyShop.Tests.Mocks
+{
+    public class MockSessionManager : ISessionManager
+    {
+        public User? CurrentUser { get; set; }
+        public string? Token { get; set; }
+
+        // Derived property logic for testing
+        public bool IsAuthenticated => !string.IsNullOrEmpty(Token) && CurrentUser != null;
+
+        public void ClearSession()
+        {
+            CurrentUser = null;
+            Token = null;
+        }
+
+        public void SaveSession(string token, User user, bool rememberMe)
+        {
+            Token = token;
+            CurrentUser = user;
+        }
+    }
+}
